9476cbbae9677854f24e068fbfe4c8060c87bc48
alprncyln58 <kullaniciadimalperen@gmail.com>
2026-05-14 10:43:34 +0300

    asdasda

--- src/App.tsx
+++ src/App.tsx
@@ -454,7 +454,7 @@ export default function App() {
     if (!movie) return "";
     const type = movie.media_type === 'tv' || (!movie.media_type && !movie.title) ? 'tv' : 'movie'; 
     const id = movie.id;
-    return `https://vidsrc-embed.ru/embed/movie?tmdb=${type}/${id}`;
+    return `https://vidsrc.to/embed/tv/${type}/${id}`;
   };
 
   const getImage = (path?: string, size = 'original') => path ? `https://image.tmdb.org/t/p/${size}${path}` : null;
