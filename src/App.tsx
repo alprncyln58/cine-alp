e738fd8e5bed0306b9837db3ae782a47a0cdb632
alprncyln58 <kullaniciadimalperen@gmail.com>
2026-05-14 10:09:58 +0300

    firebase

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,12 @@ import {
 
 // --- FIREBASE AYARLARI ---
 const firebaseConfig = {
-  apiKey: "AIzaSyCVeQ-Rj8Kx-YAJtEZs7bqzLkr2hwQrNlo",
-  authDomain: "gen-lang-client-0259328350.firebaseapp.com",
-  projectId: "gen-lang-client-0259328350",
-  storageBucket: "gen-lang-client-0259328350.firebasestorage.app",
-  messagingSenderId: "632827008516",
-  appId: "1:632827008516:web:a612a17fe4d257df458751",
-  measurementId: "G-ZDYLM2DQCV"
+  apiKey: "AIzaSyA9pFHmmbGuqzlZtxT3fehN6QF25MaM-fI",
+  authDomain: "fizyoloji-egitim-pro.firebaseapp.com",
+  projectId: "fizyoloji-egitim-pro",
+  storageBucket: "fizyoloji-egitim-pro.firebasestorage.app",
+  messagingSenderId: "481623846869",
+  appId: "1:481623846869:web:8adaa127d04e385861d75c"
 };
 
 // Firebase Başlatma
